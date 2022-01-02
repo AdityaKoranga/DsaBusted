a7acb7eba2b873f6b9d608b7a7ae568d7729122b
AdityaKoranga <Aditya.Koranga@ramanujan.du.ac.in>
2022-01-02 20:09:18 +0530

    recursion started

---
+++ recursion/factorial.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int factorial(int n){
+        //base case
+        if(n==0){
+            return 1;
+        }
+        int smallerProblem=factorial(n-1);
+        return n*smallerProblem;
+    }
+int main(){
+    int n;
+    cin>>n;
+    int ans=factorial(n);
+    cout<<ans<<endl;
+return 0;
+}
