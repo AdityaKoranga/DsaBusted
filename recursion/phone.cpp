21e30396a4ee1442e53d1f6f635f21425ed45f1b
AdityaKoranga <Aditya.Koranga@ramanujan.du.ac.in>
2022-01-14 22:31:28 +0530

    done

--- recursion/phone.cpp
+++ recursion/phone.cpp
@@ -1,37 +1,37 @@
-#include<iostream>
-#include<vector>
-using namespace std;
-void solve(string digit,string output,int index,vector<string>& ans,string mapping){
-    //base case
-    if(index>=digit.length()){
-        ans.push_back(output);
-        return;
-    }
-    int number=digit[index]-'0';//exact number mil jayega
-    string value=mapping[number];
-    for(int i=0;i<value.length();i++){
-        output.push_back(value[i]);
-        solve(digit,output,index++,ans ,mapping);
-        //ab a bhi hatana padega backtacking k waqt
-        output.pop_back();
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// void solve(string digit,string output,int index,vector<string>& ans,string mapping){
+//     //base case
+//     if(index>=digit.length()){
+//         ans.push_back(output);
+//         return;
+//     }
+//     int number=digit[index]-'0';//exact number mil jayega
+//     string value=mapping[number];
+//     for(int i=0;i<value.length();i++){
+//         output.push_back(value[i]);
+//         solve(digit,output,index++,ans ,mapping);
+//         //ab a bhi hatana padega backtacking k waqt
+//         output.pop_back();
         
-    }
+//     }
 
-}
-vector<string>letterCombination(string digits){
-    vector<string> ans;
-    if(digits.length()==0)
-    return ans;
-    string output;
-    int index=0;
-    //mapping create karni padegi
-    string mapping[10]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-    solve(digits,output,index,ans,mapping);
-    return ans;
-}
-int main(){
+// }
+// vector<string>letterCombination(string digits){
+//     vector<string> ans;
+//     if(digits.length()==0)
+//     return ans;
+//     string output;
+//     int index=0;
+//     //mapping create karni padegi
+//     string mapping[10]={"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+//     solve(digits,output,index,ans,mapping);
+//     return ans;
+// }
+// int main(){
 
 
     
-return 0;
-}   
+// return 0;
+// }   
